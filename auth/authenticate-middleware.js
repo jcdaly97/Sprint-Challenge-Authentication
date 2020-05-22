b74b2ac8c1aebb8d68f4cc2e0c9ac3dfe9318a37
John Daly <jcdaly97@gmail.com>
2020-05-22 08:49:47 -0700

    added auth endpoints and middleware, now for testing

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,24 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization
+  
+  if(token){
+    const secret = 'shhhhhhhhhh'
+
+    jwt.verify(token, secret, (error, decodedToken)=>{
+      if(error){
+        res.status(401).json({ message: "you shall not pass!" })
+      }else{
+        req.jwt = decodedToken
+        next()
+      }
+    })
+  }else{
+    res.status(401).json({ message: 'please provide authentication info' });
+  }
+  
 };
